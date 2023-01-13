82f4d07d97fb507ae11e15b7567f83caf9017824
rajupalumaru <raju.palumaru94@gmail.com>
2023-01-13 08:19:05 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React jangili
+          Learn React raju tech
         </a>
       </header>
     </div>
